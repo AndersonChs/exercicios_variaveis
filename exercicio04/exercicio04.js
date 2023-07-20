20c00ebbf03529bfd7277c6d5a8db2f209a12fd0
AndersonChs <anderson.chs@outlook.com>
2023-07-20 10:54:14 -0300

    att

--- exercicio04/exercicio04.js
+++ exercicio04/exercicio04.js
@@ -1,6 +1,6 @@
 let capital = 1000;
 let taxaJuros = 12.5 / 100;
 let tempo = 5
-let montante = capital + ((1 + taxaJuros) * tempo);
+let montante = capital((1 + taxaJuros) * tempo);
 
 console.log(montante);
